5bb6b43156e0b518f594ca20de4b2900c27c687f
earth <earth@earthdembp>
2020-04-12 02:26:49 +0100

    Initial Commit

---
+++ 24 point/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  24 point
+//
+//  Created by earth on 2020/4/12.
+//  Copyright © 2020 earth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 24 pointTests/_4_pointTests.swift
@@ -0,0 +1,34 @@
+//
+//  _4_pointTests.swift
+//  24 pointTests
+//
+//  Created by earth on 2020/4/12.
+//  Copyright © 2020 earth. All rights reserved.
+//
+
+import XCTest
+@testable import _4_point
+
+class _4_pointTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
